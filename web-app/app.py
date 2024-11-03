5c4985c011e271c6920f84a4b86511a7ce70c958
Sasha2810 <95037701+Sasha2810@users.noreply.github.com>
2024-11-04 01:08:00 +0700

    Add files via upload

--- web-app/app.py
+++ web-app/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def main():
+    return render_template('welcome.html')
+
+@app.route('/generate')
+def generate():
     return render_template('index.html')
 
 @app.route('/get_data',  methods=['POST'])
